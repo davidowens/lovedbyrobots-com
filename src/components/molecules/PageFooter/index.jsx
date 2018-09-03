930f5c86bbca922bf6462ab1894ab6658ac6aa34
David Owens <mail@davidowens.net>
2018-09-03 21:28:12 +0200

    favicons

--- src/components/molecules/PageFooter/index.jsx
+++ src/components/molecules/PageFooter/index.jsx
@@ -4,6 +4,32 @@ import Copyright from '../Copyright';
 const PageFooter = () => (
   <footer>
     <Copyright />
+    <div>
+      Icons made by
+      {' '}
+      <a
+        href="https://www.flaticon.com/authors/smashicons"
+        title="Smashicons"
+      >
+        Smashicons
+      </a>
+      {' '}
+      from
+      {' '}
+      <a
+        href="https://www.flaticon.com/"
+        title="Flaticon">www.flaticon.com
+      </a>
+      {' '}
+      is licensed by
+      {' '}
+      <a
+        href="http://creativecommons.org/licenses/by/3.0/"
+        title="Creative Commons BY 3.0"
+      >
+        CC 3.0 BY
+      </a>
+    </div>
   </footer>
 );
 
